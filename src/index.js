d7f902f355d6c070fa0b9853449571c92662fa16
Zane St. John <zstjohn@stanford.edu>
2022-06-20 14:55:04 -0700

    add typing and config + style properties

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-import React, { useRef, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import jdenticon from 'jdenticon';
+import React, { useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import jdenticon from "jdenticon";
 
-const Jdenticon = ({ value = 'test', size = '100%' }) => {
+function Jdenticon({ size = "100%", value, style, config }) {
+  // ref for icon svg
   const icon = useRef(null);
+
   useEffect(() => {
+    // configure jdenticon based on given config, if available
+    // (idea from https://github.com/slavik0329/react-jdenticon)
+    config && jdenticon.configure(config);
+  }, [config]);
+
+  useEffect(() => {
+    // update icon based on value
     jdenticon.update(icon.current, value);
   }, [value]);
 
   return (
-    <div>
+    <div style={style}>
       <svg data-jdenticon-value={value} height={size} ref={icon} width={size} />
     </div>
   );
-};
+}
 
 Jdenticon.propTypes = {
+  value: PropTypes.string.isRequired,
   size: PropTypes.string,
-  value: PropTypes.string.isRequired
+  style: PropTypes.object,
+  config: PropTypes.object,
 };
+
 export default Jdenticon;
---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+export default function Jdenticon({
+  value,
+  size,
+  style,
+  config,
+}: {
+  value: string;
+  size?: string;
+  style?: React.CSSProperties;
+  config?: import("jdenticon").JdenticonConfig;
+}): React.FC;
