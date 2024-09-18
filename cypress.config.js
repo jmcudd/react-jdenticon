f6a9c6ce316b4537061f66e052b256e63bb32db7
John Cudd <jmcudd@gmail.com>
2024-09-17 23:04:24 -0400

    Updated cypresss config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
     baseUrl: "http://localhost:8080",
     video: false,
     supportFile: false, // Disable the support file
+    specPattern: "cypress/integration/**/*.js", // Update this to match your test file location and naming convention
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
